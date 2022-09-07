e607c9a0a8b63717d6ef1c3410689fa6e4b48ea5
IgorZd <i.zdanevich@cthings.co>
2022-09-07 23:00:31 +0300

    hotfix

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 // import config from "config";
 import timeout from "connect-timeout";
 
-// import { runDb } from "./repositories/db";
+import { runDb } from "./repositories/db";
 import { kanbanBoardRouter } from "./routes/kanban-board-router";
 
 const app = express();
@@ -13,9 +13,9 @@ const {
   env: { PORT, MONGO_URL, NODE_ENV },
 } = process;
 const port = PORT || 3000;
-const dbUrl =
-  MONGO_URL ||
-  "mongodb+srv://admin:31qyZRfkjkpE5E0h@zdanevich-incubator.sy4sfvr.mongodb.net/Zdanevich-Incubator?retryWrites=true&w=majority?authSource=admin";
+// const dbUrl =
+//   MONGO_URL ||
+//   "mongodb+srv://admin:31qyZRfkjkpE5E0h@zdanevich-incubator.sy4sfvr.mongodb.net/test?authSource=admin";
 
 // const dbConfig: string = config.get("KanbanBoard.dbConfig.dbName");
 
@@ -27,16 +27,15 @@ const dbUrl =
 //   .catch((err) => {
 //     console.log("Database not connected" + err);
 //   });
-mongoose
-  .connect(`${dbUrl}`)
-  .then(() => console.log(`Database connected at ${dbUrl}`))
-  .catch((err) => {
-    console.log("Database not connected" + err);
-  });
+// mongoose
+//   .connect(`${dbUrl}`)
+//   .then(() => console.log(`Database connected at ${dbUrl}`))
+//   .catch((err) => {
+//     console.log("Database not connected" + err);
+//   });
 
 const parserMiddleware = bodyParser({});
 const corsMiddleware = cors();
-
 app.use(timeout("5s"));
 app.use(parserMiddleware);
 app.use(corsMiddleware);
@@ -49,11 +48,11 @@ app.use("/kanbanBoard", kanbanBoardRouter);
 
 if (NODE_ENV === "production") {
   console.log("node environment is production");
-  app.use(express.static("client/dist"));
+  app.use(express.static("/dist"));
 }
 
 const startApp = async () => {
-  // await runDb();
+  await runDb();
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
