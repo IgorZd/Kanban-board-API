e94e0fa2402e73d5678bb787c73d48ef34680b28
IgorZd <i.zdanevich@cthings.co>
2022-09-07 19:32:00 +0300

    add: mogoose is connected

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
-import { runDb } from "./repositories/db";
+// import { runDb } from "./repositories/db";
 import { kanbanBoardRouter } from "./routes/kanban-board-router";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:31qyZRfkjkpE5E0h@zdanevich-incubator.sy4sfvr.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log("Database not connected" + err);
+  });
 
 const parserMiddleware = bodyParser({});
 const corsMiddleware = cors();
@@ -21,7 +33,7 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/kanbanBoard", kanbanBoardRouter);
 
 const startApp = async () => {
-  await runDb();
+  // await runDb();
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
